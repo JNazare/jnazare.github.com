ccb994c9dae38e9c8e0ddd299154de324129780f
Juliana Nazare <jules.nazare@gmail.com>
2018-03-20 16:53:56 -0600

    adding new contents

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+function selectNavBtn(btn_id){
+	$("li.active").removeClass("active");
+	$(btn_id).addClass("active");
+}
+
+$(function(){
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+        var os = $('.first-row').offset().top; // pixels to the top of first-row
+        if(scroll > os){
+            $(".nav").addClass("bottom-nav-border");
+        }
+        else if (scroll < os){
+        	$(".nav").removeClass("bottom-nav-border");
+        }
+    });
+});
+
+$(".nav-link").click(function(){
+	selectNavBtn(this);
+});
